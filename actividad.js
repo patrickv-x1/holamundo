772d38aace859bae69078d260ccc068c1e5a8be1
patrickv-x1 <patrick.vergara183@gmail.com>
2026-04-09 20:33:19 -0400

    Add files via upload

---
+++ actividad.js
@@ -0,0 +1,11 @@
+function hola() {
+
+    alert("Hola Mundo");
+
+    document.getElementById("p1").innerHTML = "Hola Mundo";
+
+    console.log("Hola Mundo");
+
+    document.getElementById("d1").innerHTML = "Hola Mundo";
+
+}
